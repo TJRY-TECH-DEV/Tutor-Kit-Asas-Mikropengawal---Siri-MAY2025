4663e16e2b4e4bd30cdbe749015aa2e64d6d45f2
TJRY-TECH-DEV <tjry.tech.dev@gmail.com>
2025-05-03 10:57:28 +0800

    Add files via upload

---
+++ Servo_Motor/Servo_Motor.ino
@@ -0,0 +1,41 @@
+//Untuk membolehkan kawalan servo motor, aturcara ini perlu menyertakan pustaka (library) Servo
+//Aturcara ini akan memutarkan servo motor dari sudut 0 darjah ke 180 darjah dengan sudut petambahan 10 darjah
+//Kemudian, dari sudut 180 ke 0 semula dengan nilai pengurangan 10 darjah
+
+#include <Servo.h>    //sertakan pustaka Servo 
+
+Servo servomotor;   //cipta satu objek dari pustaka Servo bernama "servomotor"
+
+int sudut = 0;  //deklarasi pemboleh ubah yang dinamakan sebagai "sudut" - untuk menyimpan nilai sudut
+
+void setup() {
+  // put your setup code here, to run once:
+  servomotor.attach(9);   //Pin 9 digunakan untuk mengawal servo motor
+  servomotor.write(0);  //setkan posisi servo motor pada 0 darjah
+  delay(3000);          //tunggu 3 saat untuk memberi masa servo motor setkan kedudukan pada 0 darjah
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //sudut = 0 : Nilai awalan sudut dijadikan = 0
+  //sudut <= 180 : ulang arahan-arahan didalam gelung selagi sudut tidak melebihi 180 darjah
+  //sudut += 10 : Setelah selesai arahan-arahan didalam gelung, nilai sudut meningkat 10 darjah 
+  for(sudut = 0 ; sudut <= 180 ; sudut += 10 )  
+  {
+    servomotor.write(sudut);    //servo motor diputarkan ke nilai sudut terkini
+    delay(500);                 //tunggu 0.5 saat sebelum tambah nilai sudut
+  }//nilai sudut meningakat 10 darjah dan arahan-arahan dalam gelung ini diulang selagi sudut tidak melebihi 180
+  //jika nilai sudut sudah melebihi 180 darjah, arahan dalam gelung tidak lagi dilaksanakan, dan arahan dibawah pula dilaksanakan
+
+  //sudut = 180 : Nilai awalan sudut dijadikan = 180
+  //sudut >= 0 : Ulang arahan-arahan didalam gelung selagi nilai sudut lebih besar atau sama 0
+  //sudut -= 10 : Setelah selesai arahan-arahan didalam gelung, nilai sudut berkurnang 10 darjah 
+  for(sudut = 180 ; sudut >= 0 ; sudut -= 10 )  
+  {
+    servomotor.write(sudut);    //servo motor diputarkan ke nilai sudut terkini
+    delay(500);                 //tunggu 0.5 saat sebelum tambah nilai sudut
+  }//nilai sudut meningakat 10 darjah dan arahan-arahan dalam gelung ini diulang selagi sudut tidak melebihi 180
+  //jika nilai sudut sudah menjadi kurang dari 0 darjah, arahan dalam gelung tidak lagi dilaksanakan, dan arahan seterusnya dilaksanakan
+
+}
